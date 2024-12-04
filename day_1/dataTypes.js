adbb1130124f1427e17021993d0e44030e03b8d4
Parichay Dutta Biswas <69819505+Parichay1811@users.noreply.github.com>
2024-12-04 22:09:48 +0530

    day_1

---
+++ day_1/dataTypes.js
@@ -0,0 +1,15 @@
+/*
+Number
+String
+Boolean
+undefined
+null
+object
+BigInt
+*/
+// let b
+// console.log(b);
+function sum(a,b) {
+    return a + b; //if we would forget to return the value then it would return undefined.
+}
+console.log(sum(30,40));
---
+++ day_1/script.js
@@ -0,0 +1,11 @@
+console.log("hello world This is Parichay");
+
+let a = 10  //declearation + initialization using let
+console.log(a);
+var c = 20  //declearation + initialization using var
+console.log(c);
+const d = 30  //initialization using const. Declearation of const is not allowed in this case
+
+a = 25 //reinitialization or assignment
+
+let b //declearation
